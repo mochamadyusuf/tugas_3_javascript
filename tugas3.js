599778786a27d59cc9442957be12a14989809a5e
mochamadyusuf <mochamadd.yusuf@gmail.com>
2022-06-25 14:29:11 +0700

    tugas 3 javascript niomic

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+function luasPersegiPangjang(p, l) {
+  return p * l;
+}
+
+// sebelum diubah nilai dari variabel
+let sebelum = luasPersegiPangjang(10, 5);
+console.log(sebelum);
+
+// sesudah diubah nilai dari variabel
+let sesudah = luasPersegiPangjang(15, 10);
+console.log(sesudah);
